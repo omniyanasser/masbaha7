e7285f7163cd13c6f419b541b12a574c8df8264e
omniyanasser <132849832+omniyanasser@users.noreply.github.com>
2024-04-25 15:46:07 +0200

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,247 @@
+//1
+var counter1 =0;
+
+function increament1(){
+  ++counter1;
+  document .getElementById("incre1").innerHTML = counter1;
+};
+
+
+
+function reset1(){
+    counter1=0;
+    document .getElementById("incre1").innerHTML = counter1;
+}
+
+// 2
+
+var counter2 =0;
+
+function increament2(){
+  ++counter2;
+  document .getElementById("incre2").innerHTML = counter2;
+};
+
+function reset2(){
+    counter2=0;
+    document .getElementById("incre2").innerHTML = counter2;
+}
+
+//3
+
+var counter3 =0;
+
+function increament3(){
+  ++counter3;
+  document .getElementById("incre3").innerHTML = counter3;
+};
+
+function reset3(){
+    counter3=0;
+    document .getElementById("incre3").innerHTML = counter3;
+}
+
+// 4
+var counter4 =0;
+
+function increament4(){
+  ++counter4;
+  document .getElementById("incre4").innerHTML = counter4;
+};
+
+function reset4(){
+    counter4=0;
+    document .getElementById("incre4").innerHTML = counter4;
+}
+// 5
+var counter5 =0;
+
+function increament5(){
+  ++counter5;
+  document .getElementById("incre5").innerHTML = counter5;
+};
+
+function reset5(){
+    counter5=0;
+    document .getElementById("incre5").innerHTML = counter5;
+}
+// 6
+var counter6 =0;
+
+function increament6(){
+  ++counter6;
+  document .getElementById("incre6").innerHTML = counter6;
+};
+
+function reset6(){
+    counter6=0;
+    document .getElementById("incre6").innerHTML = counter6;
+}
+
+// 7
+var counter7 =0;
+
+function increament7(){
+  ++counter7;
+  document .getElementById("incre7").innerHTML = counter7;
+};
+
+function reset7(){
+    counter7=0;
+    document .getElementById("incre7").innerHTML = counter7;
+}
+
+// 8
+var counter8 =0;
+
+function increament8(){
+  ++counter8;
+  document .getElementById("incre8").innerHTML = counter8;
+};
+
+function reset8(){
+    counter8=0;
+    document .getElementById("incre8").innerHTML = counter8;
+}
+
+// 9
+var counter9 =0;
+
+function increament9(){
+  ++counter9;
+  document .getElementById("incre9").innerHTML = counter9;
+};
+
+function reset9(){
+    counter9=0;
+    document .getElementById("incre9").innerHTML = counter9;
+}
+
+// 10
+var counter10 =0;
+
+function increament10(){
+  ++counter10;
+  document .getElementById("incre10").innerHTML = counter10;
+};
+
+function reset10(){
+    counter10=0;
+    document .getElementById("incre10").innerHTML = counter10;
+}
+
+// 11
+var counter11 =0;
+
+function increament11(){
+  ++counter11;
+  document .getElementById("incre11").innerHTML = counter11;
+};
+
+function reset11(){
+    counter11=0;
+    document .getElementById("incre11").innerHTML = counter11;
+}
+
+// 12
+var counter12 =0;
+
+function increament12(){
+  ++counter12;
+  document .getElementById("incre12").innerHTML = counter12;
+};
+
+function reset12(){
+    counter12=0;
+    document .getElementById("incre12").innerHTML = counter12;
+}
+
+// 13
+var counter13 =0;
+
+function increament13(){
+  ++counter13;
+  document .getElementById("incre13").innerHTML = counter13;
+};
+
+function reset13(){
+    counter13=0;
+    document .getElementById("incre13").innerHTML = counter13;
+}
+
+// 14
+var counter14 =0;
+
+function increament14(){
+  ++counter14;
+  document .getElementById("incre14").innerHTML = counter14;
+};
+
+function reset14(){
+    counter14=0;
+    document .getElementById("incre14").innerHTML = counter14;
+}
+
+// 15
+var counter15 =0;
+
+function increament15(){
+  ++counter15;
+  document .getElementById("incre15").innerHTML = counter15;
+};
+
+function reset15(){
+    counter15=0;
+    document .getElementById("incre15").innerHTML = counter15;
+}
+
+// 16
+var counter16 =0;
+
+function increament16(){
+  ++counter16;
+  document .getElementById("incre16").innerHTML = counter16;
+};
+
+function reset16(){
+    counter16=0;
+    document .getElementById("incre16").innerHTML = counter16;
+}
+
+// 17
+var counter17 =0;
+
+function increament17(){
+  ++counter17;
+  document .getElementById("incre17").innerHTML = counter17;
+};
+
+function reset17(){
+    counter17=0;
+    document .getElementById("incre17").innerHTML = counter17;
+}
+
+// 18
+var counter18 =0;
+
+function increament18(){
+  ++counter18;
+  document .getElementById("incre18").innerHTML = counter18;
+};
+
+function reset18(){
+    counter18=0;
+    document .getElementById("incre18").innerHTML = counter18;
+}
+
+
+
+
+
+
+
+
+
+
+
+
